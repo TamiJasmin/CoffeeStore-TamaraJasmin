89f7f267d77f81d5534ae2c47fee42542a292646
TamiJasmin <72156854+TamiJasmin@users.noreply.github.com>
2023-09-27 15:38:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
-
+import backgroundImage from "./images/coffeeBackground.jpg";
 import NavBar from "./componentes/NavBar";
-import Home from "./componentes/Home";
 import Carrito from "./componentes/Carrito";
-import backgroundImage from './images/coffeeBackground.jpg';
-
-import Box from '@mui/material/Box';
-
-import Typography from '@mui/material/Typography';
-
-import Container from '@mui/material/Container';
+import Home from "./componentes/Home";
 import { Grid } from "@mui/material";
-
-
 
 const containerStyles = {
   backgroundImage: `url(${backgroundImage})`,
-  backgroundSize: 'cover', 
-  backgroundPosition: 'center', 
-  backgroundRepeat: 'no-repeat',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
   height: "100vh",
-  backgroundColor: 'rgba(255, 255, 255, 0.3)'
-  
-  
+  backgroundColor: "rgba(255, 255, 255, 0.3)",
 };
 
 function App() {
   return (
-
-    <Grid sx={{height: "100vh"}}>
-      <Box sx={containerStyles}>
-       <NavBar />
-       <Home />
-       <Carrito />
-
-       <Typography>
-        
-       </Typography>
-    </Box>
+    <Grid sx={containerStyles}>
+      <NavBar />
+      <Carrito />
+      <Home />
     </Grid>
-    
-     
-      
-    
   );
 }
 
