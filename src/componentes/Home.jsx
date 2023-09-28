6f2cb50a27fcfa87e58698ef5170eecc14e9f414
TamiJasmin <72156854+TamiJasmin@users.noreply.github.com>
2023-09-27 22:18:53 -0300

    Update Home.jsx

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,19 +1,1 @@
 
-import Typography from '@mui/material/Typography';
-import { Box } from "@mui/material";
-
-
-
-
-const Home = () => {
-    return (
-
-        <Box sx={{width: "100"}}>
-            <Typography>
-                Hi!
-            </Typography>
-        </Box>
-    );
-  }
-  
-  export default Home;
